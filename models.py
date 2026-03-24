3783045886635556d71472f4563d3b78dd7b1f96
Arjun Pal <a9666653@gmail.com>
2026-03-24 10:30:02 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, EmailStr
-from typing import Optional
-# pydantic is used for automatic data validation of user
-class Student(BaseModel):
-    id:int
-    name:str
-    age:int
-    course:str
-    email:Optional[EmailStr]=None
